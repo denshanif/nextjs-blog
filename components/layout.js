8b337b52dae78f1f2aef029f75c8c0465c06a450
Hanif <alfatoni922@gmail.com>
2021-03-18 09:12:18 +0700

    update

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Hanif Al Fathoni'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Hanif Al Fathoni Blog'
 
 export default function Layout({ children, home }) {
   return (
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Hanif Al Fathoni Blog"
         />
         <meta
           property="og:image"
